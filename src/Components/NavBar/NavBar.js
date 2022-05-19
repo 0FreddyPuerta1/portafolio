ca5f961fdbf8f2b96f5a2470ec7b4672629a2306
Freddy <puertafreddy24@gmail.com>
2022-05-19 14:15:59 -0500

    updated - Responsive desing

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,17 @@
 import './NavBar.css'
 import Logo from '../../assets/Logo.png';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
 
 
 
 const NavBar = ({home, about, skills, projects, aurora, contact, otherPage}) => {
+    const [hide, setHide] = useState(true);
     return(
-        <div className="NavBar">
+        <div>
+        
+        <div className={`NavBar ${hide ? 'hide':'visible'}`}>
             <ul>
                 <li><a href='/' className={home}>0. Home</a></li>
                 <li><a href='/AboutMe' className={about}>1. About Me</a></li>
@@ -14,7 +20,10 @@ const NavBar = ({home, about, skills, projects, aurora, contact, otherPage}) => 
                 <li><a href='AuroraProject' className={aurora}>4. Aurora Project</a></li>
                 <li><a href='Contact' className={contact}>5. Contact</a></li>
             </ul>
-            <img src={Logo} alt="" />
+            <img className='logo' src={Logo} alt="" />
+            <FontAwesomeIcon className='icon' icon = {faBarsStaggered} size='xl' onClick={()=>setHide(!hide)}/>
+        </div>
+        
         </div>
     );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,9 +9,27 @@ const Home = () => {
         <div className='Home bg'>
             <NavBar
             home={'selected'}/>
+            <div className='main'>
             <header>
                 <img className='bigLogo' src={BigLogo} alt=''/>
             </header>
+            <div className='text'>
+                <p>Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut 
+                    aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit 
+                    in voluptate velit esse cillum dolore eu 
+                    fugiat nulla pariatur. Excepteur sint 
+                    occaecat cupidatat non proident, sunt 
+                    in culpa qui officia deserunt mollit 
+                    anim id est laborum</p>
+            </div>
+            <button className='downloadCv'>Dowload</button>
+            </div>
         </div>
     );
 }
